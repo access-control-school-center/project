6625ea300ec24cd34cb7cb4c4ee1d3932f91fcc1
João Daniel <jotaf.daniel@gmail.com>
2022-03-23 11:25:35 -0300

    add initial version of search page
    it still needs real implementation for fetching
    
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,6 +1,79 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+import useAxiosPrivate from '../hooks/useAxiosPrivate'
+
 const Search = () => {
+  const axios = useAxiosPrivate()
+
+  const [name, setName] = useState('')
+  const [nameEmpty, setNameEmpty] = useState(true)
+  const [people, setPeople] = useState([
+
+  ])
+  const [emptyPeopleList, setEmptyPeopleList] = useState(true)
+
+  const search = async () => {
+    // TODO: real fetching logic
+
+    setPeople([{
+      id: 1,
+      name: "José Pedro"
+    },
+    {
+      id: 5,
+      name: "Maria José"
+    }])
+  }
+
+  useEffect(() => {
+    setNameEmpty(name.length == 0)
+  }, [name])
+
+  useEffect(() => {
+    setEmptyPeopleList(people.length == 0)
+  }, [people])
+
   return (
-    <p>Search page</p>
+    <section className="card">
+      <h2>Busca</h2>
+
+      <input
+        type="text"
+        className="last"
+        autoComplete="off"
+        placeholder="nome da pessoa"
+        required
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <button
+        disabled={nameEmpty}
+        onClick={search}
+      >
+        Buscar
+      </button>
+
+      {
+        emptyPeopleList ||
+        <>
+          <hr />
+
+          <h3>Resultado da busca</h3>
+
+          <ul>
+            {people.map(({ id, name }) => (
+              <li key={id}>
+                {id} - <Link to={`/user/${id}`}>{name}</Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      }
+
+
+    </section>
   )
 }
 
