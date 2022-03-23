9d61cdd8e9449d85d9001a4a07142501a0f60df4
João Daniel <jotaf.daniel@gmail.com>
2022-03-23 14:44:37 -0300

    wrap search page
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -3,54 +3,105 @@ import { Link } from "react-router-dom"
 
 import useAxiosPrivate from '../hooks/useAxiosPrivate'
 
+const CPF = "CPF",
+  ID_CEIP = "ID-CEIP",
+  NOME = "Nome",
+  N_USP = "Nº USP",
+  RG = "RG"
+
 const Search = () => {
   const axios = useAxiosPrivate()
 
-  const [name, setName] = useState('')
-  const [nameEmpty, setNameEmpty] = useState(true)
-  const [people, setPeople] = useState([
+  const possibleFilters = [
+    CPF, ID_CEIP, NOME, N_USP, RG
+  ]
 
-  ])
+  const [selectedFilter, setSelectedFilter] = useState('ID-CEIP')
+  const [given, setGiven] = useState('')
+  const [people, setPeople] = useState([])
   const [emptyPeopleList, setEmptyPeopleList] = useState(true)
+  const [disabled, setDisabled] = useState(true)
 
-  const search = async () => {
-    // TODO: real fetching logic
+  const buildParams = () => {
+    switch (selectedFilter) {
+      case CPF:
+        return {
+          "document.type": "CPF",
+          "document.value": given
+        }
 
-    setPeople([{
-      id: 1,
-      name: "José Pedro"
-    },
-    {
-      id: 5,
-      name: "Maria José"
-    }])
+      case ID_CEIP:
+        return { "id": given }
+
+      case NOME:
+        return { "name": given }
+
+      case N_USP:
+        return { "credential.nusp": given }
+
+      case RG:
+        return {
+          "document.type": "RG",
+          "document.value": given
+        }
+    }
   }
 
-  useEffect(() => {
-    setNameEmpty(name.length == 0)
-  }, [name])
+  const search = async () => {
+    if (disabled) return
+
+    setDisabled(true)
+
+    try {
+      const response = await axios.get("/users", {
+        params: buildParams()
+      })
+
+      setPeople(response.data.map(({ id, name }) => ({ id, name })))
+    } catch (err) {
+      console.log(err)
+      setPeople([])
+    } finally {
+      setDisabled(false)
+    }
+  }
 
   useEffect(() => {
     setEmptyPeopleList(people.length == 0)
   }, [people])
 
+  useEffect(() => {
+    setDisabled(given.length == 0)
+  }, [given])
+
   return (
     <section className="card">
       <h2>Busca</h2>
+
+      <select
+        value={selectedFilter}
+        onChange={(e) => setSelectedFilter(e.target.value)}
+        required
+      >
+        {possibleFilters.map((filter) => (
+          <option key={filter} value={filter}>{filter}</option>
+        ))}
+      </select>
 
       <input
         type="text"
         className="last"
         autoComplete="off"
-        placeholder="nome da pessoa"
+        placeholder={`${selectedFilter} da pessoa`}
+        value={given}
+        onChange={(e) => setGiven(e.target.value)}
         required
-        value={name}
-        onChange={(e) => setName(e.target.value)}
       />
 
       <button
-        disabled={nameEmpty}
         onClick={search}
+        disabled={disabled}
+        className={disabled ? "disabled" : ""}
       >
         Buscar
       </button>
@@ -65,7 +116,7 @@ const Search = () => {
           <ul>
             {people.map(({ id, name }) => (
               <li key={id}>
-                {id} - <Link to={`/user/${id}`}>{name}</Link>
+                [{id}] <Link to={`/user/${id}`}>{name}</Link>
               </li>
             ))}
           </ul>
