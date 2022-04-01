78a17ff2885b9b020d69cee8fef1a7975378b572
BrizaMel <brizamel.dias@gmail.com>
2022-04-01 14:09:11 +0000

    Complete Person class
    Signed-off-by: BrizaMel <brizamel.dias@gmail.com>

--- backend/src/main/kotlin/br/usp/ip/ceip/domain/Person.kt
+++ backend/src/main/kotlin/br/usp/ip/ceip/domain/Person.kt
@@ -1,3 +1,10 @@
 package br.usp.ip.ceip.domain
 
-class Person
+import java.time.LocalDate
+
+data class Person(
+    val name: String,
+    val documentType: String,
+    val documentValue: String,
+    val shotDate: LocalDate,
+)
