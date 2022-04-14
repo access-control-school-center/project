dbad706ffd1db52a4f01bbf9f0162d087eb9c157
BrizaMel <brizamel.dias@gmail.com>
2022-04-14 12:19:06 +0000

    Add exceptions to DocumentValidator
    Signed-off-by: BrizaMel <brizamel.dias@gmail.com>

--- backend/src/main/kotlin/br/usp/ip/ceip/domain/DocumentValidator.kt
+++ backend/src/main/kotlin/br/usp/ip/ceip/domain/DocumentValidator.kt
@@ -1,7 +1,9 @@
 package br.usp.ip.ceip.domain
 
+import br.usp.ip.ceip.domain.exceptions.ValidationException
+
 class DocumentValidator {
-    private fun removeInvalidCharacters(value: String) : String {
+    private fun removeInvalidCharactersCPF(value: String) : String {
         var cpf: String = ""
         for (c in value) {
             if (c.isDigit()) cpf += c
@@ -10,49 +12,79 @@ class DocumentValidator {
         return cpf
     }
 
-    private fun validateFirstDigit(cpf: String) : Boolean {
+    private fun removeInvalidCharactersRG(value: String) : String {
+        var cpf: String = ""
+        for (c in value) {
+            if (c.isLetterOrDigit()) cpf += c
+        }
+
+        return cpf
+    }
+
+    private fun validateFirstDigit(cpf: String) {
         var sum: Int = 0
         var rest: Int
 
         for (i in 1..9) {
-            sum += cpf.get(i-1).toString().toInt() * (11 - i)
+            sum += cpf[i-1].toString().toInt() * (11 - i)
         }
 
         rest = ( sum * 10 ) % 11
 
         if (rest == 10) rest = 0
 
-        if (rest != cpf.get(9).toString().toInt()) return false
-
-        return true
+        if (rest != cpf[9].toString().toInt()) {
+            throw ValidationException("Person", "document [CPF]", "invalid document value")
+        }
     }
 
-    private fun validateSecondDigit(cpf: String) : Boolean {
+    private fun validateSecondDigit(cpf: String) {
         var sum: Int = 0
         var rest: Int
 
         for (i in 1..10) {
-            sum += cpf.get(i-1).toString().toInt() * (12 - i)
+            sum += cpf[i-1].toString().toInt() * (12 - i)
         }
 
         rest = ( sum * 10) % 11
 
         if (rest == 10) rest = 0
 
-        if (rest != cpf.get(10).toString().toInt()) return false
-
-        return true
+        if (rest != cpf[10].toString().toInt()) {
+            throw ValidationException("Person", "document [CPF]", "invalid document value")
+        }
     }
 
-    private fun validateCPF(value: String) : Boolean {
-        val cpf: String = removeInvalidCharacters(value)
+    private fun validateCPF(value: String) {
+        val cpf: String = removeInvalidCharactersCPF(value)
 
-        if (cpf.length != 11) return false;
+        if (cpf.length != 11) {
+            throw ValidationException("Person", "document [CPF]", "invalid document value")
+        }
 
-        return validateFirstDigit(cpf) && validateSecondDigit(cpf)
+        validateFirstDigit(cpf)
+        validateSecondDigit(cpf)
     }
 
-    fun validateDocument(person: Person) : Boolean {
-        return validateCPF(person.documentValue)
+    private fun validateRG(value: String) {
+        val rg = removeInvalidCharactersCPF(value)
+
+        if (rg.length < 7 || rg.length > 10) {
+            throw ValidationException("Person", "document [RG]", "invalid document value")
+        }
+    }
+
+    fun validateDocument(person: Person) {
+        when (person.documentType) {
+            "RG" -> {
+                validateRG(person.documentValue)
+            }
+            "CPF" -> {
+                validateCPF(person.documentValue)
+            }
+            "UNDO" -> {
+                return
+            }
+        }
     }
 }
