8f1ff8944ca3549a3441b5ccfd6337e6703c8fcf
BrizaMel <brizamel.dias@gmail.com>
2022-04-14 12:21:23 +0000

    Add exceptions to ShotDateValidator
    Signed-off-by: BrizaMel <brizamel.dias@gmail.com>

--- backend/src/main/kotlin/br/usp/ip/ceip/domain/ShotDateValidator.kt
+++ backend/src/main/kotlin/br/usp/ip/ceip/domain/ShotDateValidator.kt
@@ -1,17 +1,18 @@
 package br.usp.ip.ceip.domain
 
+import br.usp.ip.ceip.domain.exceptions.ValidationException
 import java.time.LocalDate
 import java.time.ZoneId
 
 class ShotDateValidator {
-    private fun validateDate(date: LocalDate) : Boolean {
+    private fun validateDate(date: LocalDate) {
         val z : ZoneId = ZoneId.of("Brazil/East")
         val today : LocalDate = LocalDate.now(z)
 
-        return !date.isAfter(today)
+        if (date.isAfter(today)) throw ValidationException("Person", "shotDate", "invalid date")
     }
 
-    fun validateShotDate(shotDate: LocalDate) : Boolean {
-        return validateDate(shotDate)
+    fun validateShotDate(person: Person) {
+        validateDate(person.shotDate)
     }
 }
