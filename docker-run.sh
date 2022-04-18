f057207389cd6a0dc033a1e5b94c8d2fe882ba74
João Daniel <jotaf.daniel@gmail.com>
2022-04-18 00:21:28 -0300

    add frontend to the manual docker script
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- docker-run.sh
+++ docker-run.sh
@@ -1,18 +1,20 @@
 NETWORK_NAME=ceip
 BACKEND_IMAGE_NAME=ceip-backend:17-04
+FRONTEND_IMAGE_NAME=ceip-frontend:17-04
 BACKEND_CONFIG_PATH=backend/src/main/resources
-PG_HOST_BIND=5432
-BACKEND_HOST_BIND=8080
+BACKEND_HOST_BIND=6080
+FRONTEND_HOST_BIND=6070
 
 # create the network
 docker network create --attachable ${NETWORK_NAME}
 
 # run the database
-docker container run --name ceip-pg --network ${NETWORK_NAME} --hostname pg -v ${PWD}/data:/var/lib/postgresql/data --env-file ${PWD}/backend/.env -p ${PG_HOST_BIND}:5432 -d postgres
+docker container run --name ceip-pg --network ${NETWORK_NAME} --hostname pg -v ${PWD}/data:/var/lib/postgresql/data --env-file ${PWD}/backend/.env -d postgres
 
-# build the production image
+# build the production images
 cp ${BACKEND_CONFIG_PATH}/application.prod ${BACKEND_CONFIG_PATH}/application.conf
 docker image build ${PWD}/backend -t ${BACKEND_IMAGE_NAME}
+docker image build ${PWD}/frontend -t ${FRONTEND_IMAGE_NAME}
 
 # configure the database context
 sleep 10
@@ -21,3 +23,6 @@ docker container exec ceip-pg psql -U ceip -c "GRANT ALL PRIVILEGES ON DATABASE 
 
 # run the backend
 docker container run --name ceip-backend --network ${NETWORK_NAME} --env-file ${PWD}/backend/.env -p ${BACKEND_HOST_BIND}:8080 -d ${BACKEND_IMAGE_NAME}
+
+# run the backend
+docker container run --name ceip-frontend --network ${NETWORK_NAME} -p ${FRONTEND_HOST_BIND}:3000 -d ${FRONTEND_IMAGE_NAME}
