5487d6fd3c43b0f9ddc38eae60cb0faa4b04229f
João Daniel <jotaf.daniel@gmail.com>
2022-04-18 09:44:36 -0300

    move all configs to .env
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,6 @@
 NETWORK_NAME=ceip
 BACKEND_IMAGE_NAME=ceip-backend:17-04
 FRONTEND_IMAGE_NAME=ceip-frontend:17-04
-BACKEND_CONFIG_PATH=backend/src/main/resources
 BACKEND_HOST_BIND=6080
 FRONTEND_HOST_BIND=6070
 
@@ -12,7 +11,6 @@ docker network create --attachable ${NETWORK_NAME}
 docker container run --name ceip-pg --network ${NETWORK_NAME} --hostname pg -v ${PWD}/data:/var/lib/postgresql/data -v ${PWD}/seeds.sql:/seeds.sql --env-file ${PWD}/backend/.env -d postgres
 
 # build the production images
-cp ${BACKEND_CONFIG_PATH}/application.prod ${BACKEND_CONFIG_PATH}/application.conf
 docker image build ${PWD}/backend -t ${BACKEND_IMAGE_NAME}
 docker image build ${PWD}/frontend -t ${FRONTEND_IMAGE_NAME}
 
