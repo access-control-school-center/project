3f89e6ec6ca6c77d84ef72b8a73b841a51bae981
BrizaMel <brizamel.dias@gmail.com>
2022-04-29 00:01:35 +0000

    Add Update as part of service
    Signed-off-by: BrizaMel <brizamel.dias@gmail.com>

--- backend/src/main/kotlin/br/usp/ip/ceip/domain/Update.kt
+++ backend/src/main/kotlin/br/usp/ip/ceip/domain/Update.kt
@@ -1,2 +1,26 @@
 package br.usp.ip.ceip.domain
 
+import br.usp.ip.ceip.api.EmployeeUpdatePayload
+
+fun update (
+    id: String,
+    employeeUpdatePayload: EmployeeUpdatePayload,
+    personRepository: PersonRepository,
+    personValidator: PersonValidator
+) : Employee {
+
+    val previousEmployee = personRepository.findOneEmployeeById(id)
+    val (name, documentType, documentValue, shotDate, credential) = employeeUpdatePayload
+
+    val updatedEmployee = Employee (
+        name,
+        documentType,
+        documentValue,
+        shotDate,
+        CEIPID(id),
+        credential
+    )
+
+    personValidator.validateUpdate(previousEmployee, updatedEmployee)
+    return personRepository.save(updatedEmployee)
+}
