de4ca7e150a5ee6d9178db51ea60a2a55a04b7c5
BrizaMel <brizamel.dias@gmail.com>
2022-04-29 00:00:56 +0000

    Add EmployeeUpdatePayload
    Signed-off-by: BrizaMel <brizamel.dias@gmail.com>

--- backend/src/main/kotlin/br/usp/ip/ceip/api/EmployeeUpdatePayload.kt
+++ backend/src/main/kotlin/br/usp/ip/ceip/api/EmployeeUpdatePayload.kt
@@ -1,4 +1,16 @@
 package br.usp.ip.ceip.api
 
-class EmployeeUpdatePayload {
-}
+import br.usp.ip.ceip.domain.LocalDateSerializer
+import br.usp.ip.ceip.domain.Credential
+import kotlinx.serialization.Serializable
+import java.time.LocalDate
+
+@Serializable
+data class EmployeeUpdatePayload (
+    val name: String,
+    val documentType: String,
+    val documentValue: String,
+    @Serializable(with = LocalDateSerializer::class)
+    val shotDate: LocalDate,
+    val credential: Credential
+)
