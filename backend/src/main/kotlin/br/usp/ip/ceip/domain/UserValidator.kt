66003aa560d050c4dcffb0fe9c9f608db29a9073
BrizaMel <brizamel.dias@gmail.com>
2022-05-10 17:23:20 -0300

    Add UserValidator class
    Signed-off-by: BrizaMel <brizamel.dias@gmail.com>

--- backend/src/main/kotlin/br/usp/ip/ceip/domain/UserValidator.kt
+++ backend/src/main/kotlin/br/usp/ip/ceip/domain/UserValidator.kt
@@ -1,4 +1,26 @@
 package br.usp.ip.ceip.domain
 
-class UserValidator {
+import br.usp.ip.ceip.domain.exceptions.ValidationException
+
+class UserValidator(
+    private val personRepository: PersonRepository,
+    private val documentValidator: DocumentValidator,
+    private val shotDateValidator: ShotDateValidator,
+) : PersonValidator(
+    personRepository,
+    documentValidator,
+    shotDateValidator) {
+
+    private fun validateServices(services: Set<String>) {
+        for (s in services) {
+            if (s.isEmpty()) {
+                throw ValidationException("User", "services", "invalid service (empty string)")
+            }
+        }
+    }
+
+    fun validateUpdate(previous: User, updated: User) {
+        super.validateUpdate(previous, updated)
+        validateServices(updated.services)
+    }
 }
