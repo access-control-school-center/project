46b85aab180b3db8efbd6316538c263290129a76
BrizaMel <brizamel.dias@gmail.com>
2022-05-10 15:49:06 -0300

    Add function to validade and save updated users
    Signed-off-by: BrizaMel <brizamel.dias@gmail.com>

--- backend/src/main/kotlin/br/usp/ip/ceip/domain/Update.kt
+++ backend/src/main/kotlin/br/usp/ip/ceip/domain/Update.kt
@@ -2,8 +2,37 @@ package br.usp.ip.ceip.domain
 
 import at.favre.lib.crypto.bcrypt.BCrypt
 import br.usp.ip.ceip.api.EmployeeUpdatePayload
+import java.time.LocalDate
 
-fun update (
+fun updateUser (
+    id: String,
+    name: String,
+    documentType: String,
+    documentValue: String,
+    shotDate: LocalDate,
+    services: Set<String>,
+    personRepository: PersonRepository,
+    personValidator: PersonValidator
+) : User {
+
+    val previousUser = personRepository.findOneUserById(id)
+
+    val updatedUser = User (
+        name,
+        documentType,
+        documentValue,
+        shotDate,
+        CEIPID(id),
+        services
+    )
+
+    personValidator.validateUserUpdate(previousUser, updatedUser)
+    return personRepository.save(updatedUser)
+
+}
+
+
+fun updateEmployee (
     id: String,
     employeeUpdatePayload: EmployeeUpdatePayload, // TODO: refactor to avoid using external abstraction inside the domain
     personRepository: PersonRepository,
@@ -26,6 +55,6 @@ fun update (
         )
     )
 
-    personValidator.validateUpdate(previousEmployee, updatedEmployee, credential.nusp, credential.password, passwordHash)
+    personValidator.validateEmployeeUpdate(previousEmployee, updatedEmployee, credential.nusp, credential.password, passwordHash)
     return personRepository.save(updatedEmployee)
 }
